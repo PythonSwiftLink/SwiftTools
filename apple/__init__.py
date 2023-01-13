8da6d93290fb3a1ac9403306e18a6e29395940af
psychowaspx <psychowaspx@gmail.com>
2023-01-13 21:54:06 +0100

    .

---
+++ apple/__init__.py
@@ -0,0 +1,13 @@
+from package_tools import WrapPackage
+
+class FoundationPackage(WrapPackage):
+
+    name = "Foundation"
+    file = __file__
+    library = __module__
+    version = "1.0.0"
+    depends = [
+        
+    ]
+
+package = FoundationPackage
