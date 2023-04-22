d4ec1dc59ac90ef0b65add76e6d41a58eca78c4b
psychowaspx <psychowaspx@gmail.com>
2023-04-22 12:27:47 +0200

    ..

--- src/swift_tools/swift_types/__init__.py
+++ src/swift_tools/swift_types/__init__.py
@@ -6,7 +6,7 @@ from ctypes import c_uint
 from ctypes import c_long
 from typing import List,Tuple,TypeVar, TypeAlias, NewType
 from typing import Optional
-
+from typing import Sequence as sequence
 
 __all__ = [
     "long",
@@ -26,25 +26,39 @@ __all__ = [
     "float32",
     ## other types
     "TypeVar",
+    "NewType",
     "struct",
     "List",
     "Tuple",
+    "sequence",
+    "Array",
+    "data",
+    "NSNumber",
+
+    "func_options",
+
     "callback",
+    "callback_options"
     "call_class",
     "call_target",
     "swift_func",
     "EventDispatcher",
     "wrapper",
     "no_labels",
+    "args_rename",
+    "args_alias",
+    "no_protocol",
 
     "URL",
     "Error",
     "UUID",
-    "Optional"
+    "Optional",
+    "SwiftClass"
     ]
 
 #from ctypes import c_int8 as
 int32 = NewType("int32", int)
+uint32 = NewType("uint32", int)
 #int32 = c_int
 long = c_long
 ulong = c_ulong
@@ -55,17 +69,18 @@ short = c_int16
 int16 = c_int16
 ushort = c_uint16
 uint16 = c_uint16
-data = c_uint8
+data = NewType("data", bytes)
 json = c_int8
 jsondata = c_uint8
 uint = c_uint
 double = c_double
 float32 = c_float
 longdouble = c_longdouble
- 
+NSNumber = NewType("NSNumber",object) 
 URL = NewType("URL", str)
 Error = NewType("Error", str)
 UUID = NewType("UUID", str)
+Array = NewType("Array", sequence)
 
 
 def wrapper(py_init=True, debug_mode=False, target: str = None): ...
@@ -74,13 +89,20 @@ def wrapper(py_init=True, debug_mode=False, target: str = None): ...
 
 def callback(): ...
 
+def callback_options(delegate: str | None = None): ...
+
 def call_class(_: str): ...
 
 def call_target(_: str): ...
 
 def swift_func(): ...
 
+def func_options(**kwargs): ...
 
+def no_protocol(): ...
+
+def args_rename(**names): ...
+def args_alias(**names): ...
 
 def no_labels(**labels):
     """
@@ -124,3 +146,28 @@ def no_labels(**labels):
     ```
 
     """
+
+T = NewType("Type", type)
+
+class SwiftClass(object):
+
+    def __getattribute__(self, __name: str) -> type: ...
+
+
+    def __len__(self) -> int: ...
+    def __iter__(self) -> T: ...
+    __hash__: ClassVar[None]  # type: ignore[assignment]
+    @overload
+    def __getitem__(self, __i: SupportsIndex) -> T: ...
+    @overload
+    def __getitem__(self, __s: slice) -> list[_T]: ...
+    @overload
+    def __setitem__(self, __key: SupportsIndex, __value: _T) -> None: ...
+    @overload
+    def __setitem__(self, __key: slice, __value: Iterable[_T]) -> None: ...
+    def __delitem__(self, __key: SupportsIndex | slice) -> None: ...
+
+    def __repr__(self) -> str: ...
+
+
+        
